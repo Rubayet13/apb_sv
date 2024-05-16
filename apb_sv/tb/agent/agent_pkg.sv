1f0e2ea18c5d80f981805fa3950359ff13485efe
Rubayet <rubayetbinnasir@gmail.com>
2024-05-17 02:31:49 +0600

    Add files via upload

---
+++ apb_sv/tb/agent/agent_pkg.sv
@@ -0,0 +1,8 @@
+package agent_pkg;
+
+    `include "apb_trans.sv"
+    `include "apb_driver.sv"
+    `include "apb_generator.sv"
+    `include "apb_monitor.sv"
+
+endpackage 
---
+++ apb_sv/tb/env/env_pkg.sv
@@ -0,0 +1,5 @@
+package env_pkg;
+        import agent_pkg::*;
+        `include "apb_scb.sv"
+        `include "apb_env.sv"
+endpackage 
